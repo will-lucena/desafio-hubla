ad778c27ecc18d7cbc046ddecd4f187daf0c409b
Will Lucena <will.lucen4@gmail.com>
2023-10-24 10:42:28 -0300

    feat(backend): finish happy path methods

--- backend/controllers/sellerRepository.js
+++ backend/controllers/sellerRepository.js
@@ -1,22 +1,18 @@
 import { Seller } from "../models/seller.js";
+import { connectToDB } from "../utils/db.js";
 
-import { connectToDB, pool } from "../queries.js";
-
-export class SellersRepository {
-  constructor() {
-    connectToDB(pool);
-  }
-
-  async getAllSellers() {
-    const result = await pool.query("SELECT * FROM sellers");
-
-    return result.rows.map((row) => new Seller(row.id, row.name, row.role));
-  }
-
-  async addSeller(name, role) {
-    await pool.query("INSERT INTO sellers (name, role) VALUES ($1, $2)", [
-      name,
-      role,
-    ]);
-  }
+async function getAllSellers() {
+  client = await connectToDB();
+  const result = await client.query("SELECT * FROM sellers;");
+  return result.rows.map((row) => new Seller(row.id, row.name, row.role));
 }
+
+async function addSeller(client, name, role) {
+  const res = await client.query(
+    "INSERT INTO sellers (name, role) VALUES ($1, $2) ON CONFLICT (name) DO NOTHING;",
+    [name, role]
+  );
+  return res;
+}
+
+export { addSeller, getAllSellers };
--- backend/controllers/transactionRepository.js
+++ backend/controllers/transactionRepository.js
@@ -1,32 +1,59 @@
-import { Transaction } from "../models/transaction";
+import { Transaction } from "../models/transaction.js";
 
-import { connectToDB, pool } from "../queries";
+import { connectToDB } from "../utils/db.js";
+import { addSeller } from "./sellerRepository.js";
 
-export class TransactionsRepository {
-  constructor() {
-    connectToDB(pool);
-  }
+let client;
 
-  async getAllTransactions() {
-    const result = await pool.query("SELECT * FROM transactions");
-
-    return result.rows.map(
+async function getAllTransactions() {
+  client = await connectToDB();
+  const result = await client.query("SELECT * FROM transactions;");
+  if (result) {
+    return result?.rows.map(
       (row) =>
         new Transaction(
           row.id,
           row.kind,
           row.date,
-          row.seller_id,
-          row.transaction_value
+          row.seller_name,
+          row.transaction_value,
+          row.product_description
         )
     );
   }
-
-  async addTransaction(transaction) {
-    const { kind, date, seller_id, transaction_value } = transaction;
-    await pool.query(
-      "INSERT INTO transactions (kind, date, seller_id, transaction_value) VALUES ($1, $2, $3, $4)",
-      [kind, date, seller_id, transaction_value]
-    );
-  }
+  return [];
 }
+
+async function addTransaction(transaction, clientOpen) {
+  if (clientOpen) {
+    client = clientOpen;
+  } else {
+    client = await connectToDB();
+  }
+
+  const { kind, date, seller, value, product } = transaction;
+  if (!kind || !date || !seller || !value || !product) {
+    return;
+  }
+
+  await addSeller(client, seller, kind == 1 ? 0 : 1);
+  const result = await client.query(
+    "INSERT INTO transactions (kind, date, seller_name, transaction_value, product_description) VALUES ($1, $2, $3, $4, $5);",
+    [kind, date, seller, value, product]
+  );
+
+  if (!clientOpen) {
+    client.release();
+  }
+
+  return result;
+}
+
+async function addBatch(transactions) {
+  client = await connectToDB();
+  transactions.forEach(async (transaction) => {
+    addTransaction(transaction, client);
+  });
+}
+
+export { addBatch, addTransaction, getAllTransactions };
