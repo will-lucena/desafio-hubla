2719ee9973afd18f896cdeef48f66c6178d506d0
Will Lucena <will.lucen4@gmail.com>
2023-10-24 23:18:43 -0300

    feat(db): add query wrapper and getClient methods

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -18,4 +18,21 @@ try {
   console.log("deu ruim pra criar a pool parceiro");
 }
 
-export { pool };
+// the pool will emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
+
+export const query = async (text, params) => {
+  const start = Date.now();
+  const res = await pool.query(text, params);
+  const duration = Date.now() - start;
+  console.log("executed query", { text, params, duration, rows: res.rowCount });
+  return res;
+};
+
+export const getClient = () => {
+  return pool.connect();
+};
