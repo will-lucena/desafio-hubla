4f2c50df2272d1e95fb29c7071b3a01ebd2dd4db
Will Lucena <will.lucen4@gmail.com>
2023-11-02 10:29:00 -0300

    feat(front): create file uploader component

---
+++ frontend/src/components/FileUploader.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="file_uploader">
+    <input ref="uploader" id="uploader" type="file" accept=".txt" @change="onUpload" hidden />
+
+    <label for="uploader">Carregar transações</label>
+
+    <div class="file_uploader__box">
+      <p @click="onClickOpenUploader">
+        <span v-if="hasDragnDrop">Arrasta e solta o arquivo aqui ou </span>
+        <span class="drag_n_drop__highlight">selecione</span> um arquivo
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { uploadTransactionsFile } from '@/api/transactions'
+import { ref } from 'vue'
+
+const emit = defineEmits(['success', 'fail'])
+const uploader = ref(null)
+
+const hasDragnDrop = ref(false)
+
+function onUpload() {
+  const [file] = uploader.value.files
+
+  if (file) {
+    uploadTransactionsFile(file)
+      .then(() => {
+        emit('success')
+      })
+      .catch((error) => {
+        emit('fail', error)
+      })
+  }
+}
+
+function onClickOpenUploader() {
+  uploader.value.click()
+}
+</script>
+
+<style lang="scss" scoped>
+.file_uploader {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: start;
+  gap: 1rem;
+
+  padding: 2rem;
+
+  &__box {
+    border-radius: 0.5rem;
+    border: 1px dashed white;
+    padding: 1.5rem;
+  }
+}
+
+.drag_n_drop {
+  &__highlight {
+    cursor: pointer;
+    font-weight: 700;
+    color: aqua;
+  }
+}
+</style>
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -2,59 +2,13 @@
   <div>
     <!-- <h1>{{ message }}</h1> -->
     <form>
-      <input type="file" name="" id="" @change="onUpload" />
+      <FileUploader />
     </form>
   </div>
 </template>
 
 <script setup>
-import axios from 'axios'
-import { onMounted, ref } from 'vue'
-
-const message = ref('')
-
-const apiUrl = import.meta.env.VITE_API_URL
-
-console.log({ apiUrl })
-
-axios
-  .get(apiUrl)
-  .then((response) => {
-    message.value = response.data.message
-  })
-  .catch((error) => {
-    console.log(error)
-  })
-
-function onUpload(event) {
-  let formData = new FormData()
-  const [file] = event.target.files
-  formData.append('file', file)
-  axios
-    .post(`${apiUrl}transactions`, formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
-      }
-    })
-    .then(() => {
-      loadTransactions()
-      loadBalances()
-    })
-}
-
-async function loadTransactions() {
-  message.value = (await axios.get(`${apiUrl}transactions`)).data
-}
-
-async function loadBalances() {
-  axios.get(`${apiUrl}balances`)
-}
-
-onMounted(() => {
-  loadTransactions().then(() => {
-    loadBalances()
-  })
-})
+import FileUploader from './FileUploader.vue'
 </script>
 
 <style lang="scss" scoped></style>
