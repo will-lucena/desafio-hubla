15caf9570b6e918e3ed658c202377785fea45f39
Will Lucena <will.lucen4@gmail.com>
2023-11-02 14:11:46 -0300

    feat(front): add sortby

--- frontend/src/components/TransactionsList.vue
+++ frontend/src/components/TransactionsList.vue
@@ -1,20 +1,20 @@
 <template>
   <table v-if="hasTransactions" class="table">
     <tr class="table__row">
-      <th class="table__column">Vendedor</th>
-      <th class="table__column">Produto</th>
-      <th class="table__column">Tipo</th>
-      <th class="table__column">Valor</th>
-      <th class="table__column">Data</th>
+      <th class="table__column" @click="sortBySeller">Vendedor</th>
+      <th class="table__column" @click="sortByProduct">Produto</th>
+      <th class="table__column" @click="sortByKind">Tipo</th>
+      <th class="table__column" @click="sortByValue">Valor</th>
+      <th class="table__column" @click="sortByDate">Data</th>
     </tr>
-    <tr v-for="transaction in transactions" :key="transaction.id" class="table__row">
+    <tr v-for="transaction in sortedTransactions" :key="transaction.id" class="table__row">
       <TransactionsListItem :transaction="transaction" />
     </tr>
   </table>
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import TransactionsListItem from './TransactionsListItem.vue'
 
 const props = defineProps({
@@ -22,6 +22,59 @@ const props = defineProps({
 })
 
 const hasTransactions = computed(() => props.transactions.length > 0)
+const invertSort = ref(false)
+const activeSortFunction = ref((array) => array)
+
+const sortedTransactions = computed(() => {
+  return [...props.transactions].sort(activeSortFunction.value)
+})
+
+function sortByDate() {
+  activeSortFunction.value = (a, b) => {
+    const aDate = new Date(a.date)
+    const bDate = new Date(b.date)
+
+    return invertSort.value ? aDate - bDate : bDate - aDate
+  }
+
+  invertSort.value = !invertSort.value
+}
+
+function sortByValue() {
+  activeSortFunction.value = (a, b) => {
+    return invertSort.value ? a.value - b.value : b.value - a.value
+  }
+
+  invertSort.value = !invertSort.value
+}
+
+function sortByKind() {
+  activeSortFunction.value = (a, b) => {
+    return invertSort.value ? a.kind - b.kind : b.kind - a.kind
+  }
+
+  invertSort.value = !invertSort.value
+}
+
+function sortByProduct() {
+  activeSortFunction.value = (a, b) => {
+    if (a.productDescription > b.productDescription) return invertSort.value ? -1 : 1
+    if (b.productDescription > a.productDescription) return invertSort.value ? 1 : -1
+    return 0
+  }
+
+  invertSort.value = !invertSort.value
+}
+
+function sortBySeller() {
+  activeSortFunction.value = (a, b) => {
+    if (a.sellerName > b.sellerName) return invertSort.value ? -1 : 1
+    if (b.sellerName > a.sellerName) return invertSort.value ? 1 : -1
+    return 0
+  }
+
+  invertSort.value = !invertSort.value
+}
 </script>
 
 <style lang="scss" scoped>
@@ -47,6 +100,7 @@ const hasTransactions = computed(() => props.transactions.length > 0)
     flex: 1;
     text-align: left;
     font-weight: 700;
+    cursor: pointer;
   }
 }
 </style>
