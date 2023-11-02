9d6086214a83da33dc3da6a5e55e6f53e05591d6
Will Lucena <will.lucen4@gmail.com>
2023-11-02 12:02:09 -0300

    feat(front): create balances component

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -4,24 +4,80 @@
       <FileUploader @success="onUploadSuccess" @fail="onUploadFail" />
     </form>
 
+    <div class="balances">
+      <SellerCard
+        v-for="(balance, index) in balances"
+        :key="index"
+        :balance="balance.balance"
+        :name="balance.name"
+      />
+    </div>
+
     <TransactionsList :transactions="transactions" />
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { onMounted, ref } from 'vue'
+import { loadBalances } from '../api/balances'
+import { loadTransactions } from '../api/transactions'
 import FileUploader from './FileUploader.vue'
+import SellerCard from './SellerCard.vue'
 import TransactionsList from './TransactionsList.vue'
 
 const transactions = ref([])
+const balances = ref([])
 
 function onUploadSuccess(payload) {
-  transactions.value = payload
+  loadBalances()
+    .then(({ data }) => {
+      balances.value = data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+  if (transactions.value.length > 0) {
+    loadTransactions()
+      .then(({ data }) => {
+        transactions.value = data
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  } else {
+    transactions.value = payload
+  }
 }
 
 function onUploadFail(error) {
   console.log(error)
 }
+
+onMounted(() => {
+  loadTransactions()
+    .then(({ data }) => {
+      transactions.value = data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+  loadBalances()
+    .then(({ data }) => {
+      balances.value = data
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+})
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.balances {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+</style>
---
+++ frontend/src/components/SellerCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card">
+    <p>{{ name }}</p>
+    <p>{{ convertedBalance }}</p>
+  </div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  balance: {
+    type: Number,
+    required: true
+  }
+})
+
+const convertedBalance = computed(() => {
+  const converted = (props.balance / 100).toFixed(2)
+  return `R$ ${converted}`
+})
+</script>
+
+<style lang="scss" scoped>
+.card {
+  min-width: 250px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  padding: 2rem;
+  border-radius: 0.5rem;
+  border: 1px solid white;
+}
+</style>
