db74f23a3dbef786e6371e601e35fcf2ca015417
Will Lucena <will.lucen4@gmail.com>
2023-11-03 11:51:49 -0300

    feat(front): add error interceptors

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,12 +1,32 @@
 import axios from 'axios'
 
-export const uploadInstance = axios.create({
+const uploadInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'multipart/form-data'
   }
 })
 
-export const baseInstance = axios.create({
+uploadInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      return Promise.reject(error.response.data)
+    }
+  }
+)
+
+const baseInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL
 })
+
+baseInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      return Promise.reject(error.response.data)
+    }
+  }
+)
+
+export { baseInstance, uploadInstance }
